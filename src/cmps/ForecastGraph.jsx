54d1dcafc171cab92e407937ad2e9dc87d084101
Yiftach Samocha <yiftachsamo@gmail.com>
2025-04-15 14:04:46 +0300

    Use open wheather api and display the accurate data

--- src/cmps/ForecastGraph.jsx
+++ src/cmps/ForecastGraph.jsx
@@ -1,31 +1,60 @@
-export function ForecastGraph({location}) {
-    const weekData = [
-        { day: "Sunday", temp: 17 },
-        { day: "Monday", temp: 4 },
-        { day: "Tuesday", temp: 23 },
-        { day: "Wednesday", temp: 11 },
-        { day: "Thursday", temp: 28 },
-        { day: "Friday", temp: 9 },
-        { day: "Saturday", temp: 30 }
-    ];
+import { useEffect, useState } from "react"
+
+export function ForecastGraph({ location }) {
+    const [weekData, setWeekData] = useState([])
+    const [isError, setIsError] = useState(false)
+
+    useEffect(() => {
+        if (location) getData(location)
+
+    }, [location])
+
+    async function getData(location) {
+        try {
+            const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${location.lat}&lon=${location.lon}&exclude=current,hourly,minutely,alerts&appid=09dd6e9d0593d9c9c3f79b2762aa4908&units=metric`
+            const response = await fetch(url)
+            const data = await response.json()
+            const today = new Date()
+            const days = data.daily.map((item, idx) => {
+                const date = new Date(today)
+                date.setDate(today.getDate() + idx)
+                const dayName = date.toLocaleDateString("en-US", { weekday: "long" })
+                const formattedDate = date.toLocaleDateString("en-US", { month: "short", day: "numeric" })
+                return { day: dayName, date: formattedDate, temp: item.temp.day }
+            })
+            days[0].day = 'today'
+            setWeekData(days)
+
+        } catch {
+            setIsError(true)
+        }
+
+    }
 
 
     return <section>
-        <table className="forecast-graph">
-            <tbody>
-                <tr>
-                    {weekData.map(info => {
-                        return <td>{info.day}</td>
-                    })}
-                </tr>
-                <tr>
-                    {weekData.map(info => {
-                        return <td>{info.temp}</td>
-                    })}
-                </tr>
+        {isError ? <div>Couldnt load whethear forecast</div> :
+            <table className="forecast-graph">
+                <tbody>
+                    <tr>
+                        {weekData.map(info => {
+                            return <td>{info.day}</td>
+                        })}
+                    </tr>
+                    <tr>
+                        {weekData.map(info => {
+                            return <td>{info.date}</td>
+                        })}
+                    </tr>
+                    <tr>
+                        {weekData.map(info => {
+                            return <td>{info.temp}</td>
+                        })}
+                    </tr>
 
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        }
     </section>
 
 }
